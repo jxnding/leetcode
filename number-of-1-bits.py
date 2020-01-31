3fd7c23ea65d7323c43e05a6c69714436169f8bb
Jackson Zhaoxiong Ding <zding5@u.rochester.edu>
2020-01-31 12:20:46 -0500

    counting 1's in binary is ONLY constant if we assume fixed-length ints

--- number-of-1-bits.py
+++ number-of-1-bits.py
@@ -15,5 +15,10 @@ class Solution:
         return ham(n, 0)
 Solution().hammingWeight(11)
 #### O(log n), O(1); 12, 100 Python3
-#### TODO: Can be constant, what exactly is constant?
+#### TODONE: Can be constant, what exactly is constant? | Constant requires fixed-length int
 #### Yay tail recursion!
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count('1')
+#### Another log n solution. 41, 100 Python3
