ca98eec98990f63342c71a709eb2a79eb369b3de
Jackson Zhaoxiong Ding <zding5@u.rochester.edu>
2020-04-07 13:21:53 -0400

    did optimal solution for largest rect in hist but with a lot of help, need to review

--- largest-rectangle-in-histogram.py
+++ largest-rectangle-in-histogram.py
@@ -1,5 +1,25 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        left = [-1]*len(heights) ####
+        for i in range(1, len(heights)):
+            l = i-1
+            while l>=0 and heights[l]>=heights[i]:
+                l = left[l]
+            left[i] = l
+        right = [len(heights)]*len(heights)
+        for i in range(len(heights)-2, -1, -1):
+            r = i+1
+            while r<len(heights) and heights[r]>=heights[i]:
+                r = right[r]
+            right[i] = r
+        max_area = 0
+        for i in range(len(heights)):
+            max_area = max(max_area, heights[i]*(right[i]-left[i]-1))
+        return max_area
+#### O(n), O(n); 16, 9 Python3
+# REVIEW
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         max_area = 0
         for i in range(len(heights)):
             l, r = i, i
