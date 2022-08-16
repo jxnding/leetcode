d6317958ab262611c9e074a0a5932561c4f87e24
Zhaoxiong Ding <sabot00@gmail.com>
2022-08-15 21:45:41 -0400

    # reachable-nodes-with-restrictions Started at 9:24PM
    ## Python
    Fuck this shit. They call this a "tree" but there's cycles and undirected edges.
    Honestly tho, fuck me for not just doing DFS. Never hurts to "protect" yourself with a visited set.

---
+++ reachable-nodes-with-restrictions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        def explore(start_node: int):
+            # nonlocal res_nodes
+            # nonlocal edge_list
+            ans = 1
+            for n in edge_list[start_node]:
+                if n in res_nodes: continue
+                ans += explore(n)
+            return ans
+        res_nodes = set(restricted)
+        edge_list = [[] for _ in range(n)]
+        for a, b in edges: 
+            edge_list[min(a, b)].append(max(a, b))
+        print(edge_list)
+        return explore(0)
